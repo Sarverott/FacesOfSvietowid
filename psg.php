42f0ae76a7042d8025b8fdf496b59654a7eea5e4
Adam Borucki <sarverott@gmail.com>
2018-03-29 21:43:22 +0200

    # SOURCE FILE #
    file to include

---
+++ psg.php
@@ -0,0 +1,47 @@
+<?php 
+	/*
+	** ###### PHP Selector Generator ######
+	** by Adam Borucki (Sarverott) 2018
+	** MIT Licence
+	*/
+	class namesGenotype{
+		private $nameslength;
+		private $nameseed=[];
+		public $namesspace=array();
+		public function __construct($len=10){
+			$this->nameslength=$len;
+			for($i=0;$i<$this->nameslength;$i++){
+				if($i==0){
+					$this->nameseed[]=rand(0,25);
+				}else{
+					$this->nameseed[]=rand(0,25);
+				}
+			}
+		}
+		private function letter_alg($notfirst, $x, $seed){
+			$var=0;
+			if($x%2==0){
+				$var=$seed-round($x/2);
+			}else{
+				$var=$seed+$x-round(($x-1)/2);
+			}
+			if($var<0){
+				$var+=26;
+			}elseif($var>=26){
+				$var%=26;
+			}
+			if($var<26&&$var>=0){
+				return chr($var+97);
+			}
+		}
+		public function get_name($index){
+			if(!isset($this->namesspace[$index])){
+				$this->namesspace[$index]="";
+				for($i=0;$i<$this->nameslength;$i++){
+					$this->namesspace[$index]=$this->letter_alg($i,round($index/(pow(26, $i)))%26,$this->nameseed[$i]).$this->namesspace[$index];
+				}
+			}
+			return $this->namesspace[$index];
+		}
+	}
+?>
